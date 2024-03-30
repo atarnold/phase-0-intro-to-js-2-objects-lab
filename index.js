cf7c62371c015673d8e06532d5dd31b602efff8f
arnold <arnoldkmaina@gmail.com>
2024-03-30 09:47:17 +0300

    intro-to-js-2-objects

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+     name:"Michael",
+     streetAdress:"02 street",    
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+     return {...employee, [key]: value};
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newObj = {...employee}
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
